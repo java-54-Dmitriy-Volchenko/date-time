8175f0fa4cc7d74800ba9ae5156700fe73d6e928
DmitriyVolchenko <Dmitriy.Volchenko@gmail.com>
2024-05-05 18:54:21 +0300

    My version of HW-18

--- src/telran/time/NextFriday13.java
+++ src/telran/time/NextFriday13.java
@@ -1,5 +1,7 @@
 package telran.time;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
 import java.time.temporal.Temporal;
 import java.time.temporal.TemporalAdjuster;
 
@@ -7,8 +9,15 @@ public class NextFriday13 implements TemporalAdjuster {
 
 	@Override
 	public Temporal adjustInto(Temporal temporal) {
-		// TODO Auto-generated method stub
-		return null;
+		  LocalDate date = LocalDate.from(temporal);
+		  LocalDate nextFriday = date.with(DayOfWeek.FRIDAY);
+	        if (date.getDayOfWeek() == DayOfWeek.FRIDAY && date.getDayOfMonth() < 13) {
+	            nextFriday = nextFriday.plusWeeks(1);
+	        }
+	        while (nextFriday.getDayOfMonth() != 13) {
+	            nextFriday = nextFriday.plusWeeks(1);
+	        }
+		return nextFriday;
 	}
 
 }
--- src/telran/time/test/DateTimeTest.java
+++ src/telran/time/test/DateTimeTest.java
@@ -5,11 +5,13 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.time.*;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalAdjusters;
 import java.util.Locale;
 
 import org.junit.jupiter.api.Test;
 
 import telran.time.BarMizvaAdjuster;
+import telran.time.NextFriday13;
 
 class DateTimeTest {
 
@@ -21,12 +23,31 @@ class DateTimeTest {
 	}
 	@Test
 	void nextFriday13Test() {
-		//TODO
-	}
+		  NextFriday13 nextFriday13 = new NextFriday13();
+      
+	        LocalDate testDate1 = LocalDate.of(2024, 4, 11); 
+	        LocalDate expectedDate1 = LocalDate.of(2024, 9, 13); 
+	        LocalDate result1 = testDate1.with(nextFriday13);
+	        assertEquals(expectedDate1, result1);
+
+	       
+	        LocalDate testDate2 = LocalDate.of(2024, 4, 13); 
+	        LocalDate expectedDate2 = LocalDate.of(2024, 9, 13); 
+	        LocalDate result2 = testDate2.with(nextFriday13);
+	        assertEquals(expectedDate2, result2);
+	      
+	                     
+	        LocalDate testDate4 = LocalDate.of(2024, 4, 13); 
+	        LocalDate expectedDate4 = LocalDate.of(2024, 9, 13); 
+	        LocalDate result4 = testDate4.with(nextFriday13);
+	        assertEquals(expectedDate4, result4);
+	    }
+	
 	@Test
 	void friday13RangeTest() {
 		//TODO
 	}
+	
 	@Test
 	void displayCurrentDateTimeTest() {
 		//The following test implies printing out
@@ -34,14 +55,27 @@ class DateTimeTest {
 		
 		displayCurrentDateTime("Canada");
 		
-	}
-	private void displayCurrentDateTime(String zonePart) {
-		// TODO Auto-generated method stub
-		// prints out current date/time in all zones containing zonePart
-		//format <year>-<month number>-<day> <hh:mm> <zone name>
-		//ZonedDateTime class
-		System.out.println("kuku");
-	}
+	};
 	
-
+	private void displayCurrentDateTime(String zonePart) {
+		  String[] allZoneIds = ZoneId.getAvailableZoneIds().toArray(new String[0]);
+		  for (int i = 0; i < allZoneIds.length; i++) {
+		        String zoneId = allZoneIds[i];
+		        
+		        if (zoneId.contains(zonePart)) {
+		           
+		            ZonedDateTime currentTime = ZonedDateTime.now(ZoneId.of(zoneId));
+		           
+		            String formattedTime = formatZonedDateTime(currentTime);
+		            System.out.println(formattedTime);
+		        }
+	
+		  }
+	}
+	private String formatZonedDateTime(ZonedDateTime zonedDateTime) {
+	    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("uuuu-MM-dd HH:mm z");
+	    return zonedDateTime.format(formatter);
 }
+}
+
+	
